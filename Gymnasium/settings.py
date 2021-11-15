487b8f2fe309f2dbf481f9607be5a1b8e4b5fb9c
rajparmarr2308 <rajparmar23801@gmail.com>
2021-11-15 18:35:40 +0530

    added things

--- Gymnasium/settings.py
+++ Gymnasium/settings.py
@@ -7,9 +7,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k6_^w5l$q(85$r_vc*ou9q!aa8hh_aczk425&iw6w4qx$=cg=l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://thefitlifegym.herokuapp.com/']
+ALLOWED_HOSTS = ['https://thefitlifegym.herokuapp.com']
 
 # After succesful login go to path:
 LOGIN_REDIRECT_URL = '/'
